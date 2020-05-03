bf8baae9e369b3bc38d3106393ede195f852f76d
viniciustbitencourt <vn.biten@gmail.com>
2020-05-03 14:28:57 -0300

    php teste

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 echo "<h1> Teste Azure WebAPP </h1>"; 
+$db_name="myaccountdatabasetest"
+$db_database="mydatabase"
+
+
 echo $db_name
+echo $db_database
+
+echo "Esse foi um teste de variáveis em PHP"
 
 
 ?>
