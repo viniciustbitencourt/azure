3fb92c2e53c28ca9c9b5014460325f0245dc9381
viniciustbitencourt <vn.biten@gmail.com>
2020-05-03 14:36:46 -0300

    teste php now

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ $db_name="myaccountdatabasetest";
 $db_database="mydatabase";
 
 
-echo $db_name
-echo $db_database
+echo $db_name.' esse e o database name';
+echo $db_database.' essee e o database qualquer';
 
 echo "Esse foi um teste de variáveis em PHP";
 
